2e154c34e46eb752e817e2adba6a0a547b72dec1
Mark Vayngrib <mark@tradle.io>
2016-09-27 09:47:50 -0400

    fix backoff

--- client.js
+++ client.js
@@ -5,11 +5,12 @@ var parseURL = require('url').parse
 var typeforce = require('typeforce')
 var extend = require('xtend')
 var debug = require('debug')('sendy-ws')
-var WebSocket = require('ws')
+var WebSocket = global.WebSocket || require('ws/lib/WebSocket')
 var backoff = require('backoff')
 var protobufs = require('sendy-protobufs').ws
 var schema = protobufs.schema
 var CLIENTS = {}
+var noop = function () { debugger }
 
 exports = module.exports = Client
 exports.createClient = function (opts) {
@@ -44,10 +45,7 @@ function Client (opts) {
   this._backoff.on('ready', function () {
     if (self._destroyed) return
 
-    try {
-      self._socket.close()
-    } catch (err) {}
-
+    self._closeSocket()
     self._reconnect()
   })
 
@@ -56,6 +54,7 @@ function Client (opts) {
   this._opts = extend(opts)
   this._onclose = this._onclose.bind(this)
   this._onopen = this._onopen.bind(this)
+  this._onmessage = this._onmessage.bind(this)
   if (opts.autoConnect) this.connect()
 }
 
@@ -63,45 +62,67 @@ util.inherits(Client, EventEmitter)
 exports = module.exports = Client
 
 Client.prototype.connect = function () {
+  this._backoff.reset()
   this._reconnect()
 }
 
 Client.prototype._reconnect = function () {
   if (this._connecting || this._connected || this._destroyed) return
 
-  this._connecting = true
+  printStack(this._url)
   this._openSocket()
 }
 
 Client.prototype._openSocket = function () {
-  var self = this
+  this._closeSocket()
+  this._connecting = true
   this._socket = new WebSocket(this._url/*, this._wsOptions*/)
-  this._socket.on('open', this._onopen)
-  this._socket.on('close', this._onclose)
-  this._socket.on('message', function (packet, flags) {
-    if (!flags.binary) return self.emit('error', 'ignoring non-binary message')
+  this._socket.onopen = this._onopen
+  this._socket.onclose = this._onclose
+  this._socket.onmessage = this._onmessage
+}
 
-    var dec = protobufs.decoderFor(packet)
-    if (dec === schema.Error) {
-      var error = protobufs.decode(packet)
-      if (error.type === schema.ErrorType.RecipientNotFound) {
-        self.emit('404', error.recipient)
-      } else {
-        var str = JSON.stringify(error)
-        self._debug('received error: ' + str)
-        self.emit('error', new Error(str))
-      }
+Client.prototype._onmessage = function (e, flags) {
+  var packet = global.WebSocket ? e.data : e
+  if (packet instanceof ArrayBuffer) {
+    packet = new Buffer(packet)
+  }
 
-      return
+  if (!Buffer.isBuffer(packet)) {
+    return this.emit('error', 'ignoring non-binary message')
+  }
+
+  var dec = protobufs.decoderFor(packet)
+  if (dec === schema.Error) {
+    var error = protobufs.decode(packet)
+    if (error.type === schema.ErrorType.RecipientNotFound) {
+      this.emit('404', error.recipient)
+    } else {
+      var str = JSON.stringify(error)
+      this._debug('received error: ' + str)
+      this.emit('error', new Error(str))
     }
 
-    if (dec !== schema.Packet) {
-      return self._debug('ignoring unsupported packet', packet)
-    }
+    return
+  }
 
-    var msg = protobufs.decode(packet)
-    self.emit('receive', msg)
-  })
+  if (dec !== schema.Packet) {
+    return this._debug('ignoring unsupported packet', packet)
+  }
+
+  var msg = protobufs.decode(packet)
+  this.emit('receive', msg)
+}
+
+Client.prototype._closeSocket = function () {
+  if (this._socket) {
+    try {
+      this._socket.onclose = noop
+      this._socket.close()
+    } catch (err) {}
+
+    delete this._socket
+  }
 }
 
 Client.prototype._onopen = function () {
@@ -146,11 +167,6 @@ Client.prototype.destroy = function () {
   if (this._destroyed) return
 
   this._destroyed = true
-
-  if (this._socket) {
-    this._socket.close()
-    this._socket = null
-  }
-
+  this._closeSocket()
   this.emit('destroy')
 }
